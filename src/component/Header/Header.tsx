a78d1557bb487ef9884364a869e1dc57f26babd1
Egor <121@HOME-PC>
2025-01-24 12:03:42 +0300

    Сделал скролл после навигации

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 import SimpleButton from '../SimpleButton/SimpleButton';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
 
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [scrollToId, setScrollToId] = useState<string | null>(null);
+
   const scrollById = (id) => {
-    var elemPos = document.getElementById(id)?.getBoundingClientRect().top;
-    if(elemPos == undefined)
-      elemPos = 0;
-  
-    var header = document.getElementById('header');
-  
-    var headerOffset = header?.getBoundingClientRect().height
-    if(headerOffset == undefined)
-      headerOffset = 0;
-  
-    var offset = elemPos - headerOffset - 20 + window.pageYOffset;
-  
-    window.scrollTo({top: offset, behavior:'smooth'})
-  }
-  
-  const scrollToHome = () => {
-    if(location.pathname === '/home')
+    if(id === 'home')
+    {
       window.scrollTo({top: 0, behavior:'smooth'});
-    else navigate('/home')
-  }
+      return;
+    }
+    const elem = document.getElementById(id);
+    if (elem) {
+      const elemPos = elem.getBoundingClientRect().top;
+      const header = document.getElementById('header');
+      const headerOffset = header ? header.getBoundingClientRect().height : 0;
+      const offset = elemPos - headerOffset - 20 + window.pageYOffset;
+
+      window.scrollTo({ top: offset, behavior: 'smooth' });
+    }
+  };
+  
+  const navigateHomeAndScroll = (id) => {
+    if(location.pathname === '/home')
+      scrollById(id);
+    else  {
+      setScrollToId(id);
+      navigate('/home');
+    }
+  };
+
+  useEffect(() => {
+    if(scrollToId && location.pathname === '/home') {
+      scrollById(scrollToId);
+      setScrollToId(null);
+    }
+  }, [location, scrollToId]);
 
   return (
     <header className={styles.header} id='header'>
-      <SimpleButton text='Домашняя страница' onclick={scrollToHome} />
-      <SimpleButton text='О нас' onclick={() => scrollById('descriptionStart')} />
-      <SimpleButton text='Тарифы' onclick={() => scrollById('catalog')} />
+      <SimpleButton text='Домашняя страница' onclick={() => {navigateHomeAndScroll('home')}} />
+      <SimpleButton text='О нас' onclick={() => navigateHomeAndScroll('descriptionStart')} />
+      <SimpleButton text='Тарифы' onclick={() => navigateHomeAndScroll('catalog')} />
       <SimpleButton text='Калькулятор стоимости' onclick={() => {}} />
       <SimpleButton text='Отзывы' onclick={() => {}} />
     </header>
